082a78606833657d0e8bd70b694340feb63463be
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-02-03 16:49:18 -0600

    Remove card from deck

--- dictionary exercise start file.py
+++ dictionary exercise start file.py
@@ -103,7 +103,7 @@ def deal_cards(deck, number):
             print(i)
             value_hand += deck[i]
 
-        del deck[i]
+            del deck[i]
 
         print()
         print("Value of this hand is: ", value_hand)
